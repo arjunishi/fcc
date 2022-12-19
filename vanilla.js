1671343fcef30eea48fc14cfdaab085d2741661f
arjunishi <hdlogically@gmail.com>
2022-12-19 13:00:53 +0530

    return-early-pattern

--- vanilla.js
+++ vanilla.js
@@ -1,24 +1,42 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+// const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
-function golfScore(par, strokes) {
-  // Only change code below this line
-  if (strokes == 1) {
-    return names[0];
-  } else if (strokes <= par - 2) {
-    return names[1];
-  } else if (strokes === par - 1) {
-    return names[2];
-  } else if (strokes === par) {
-    return names[3];
-  } else if (strokes === par + 1) {
-    return names[4];
-  } else if (strokes === par + 2) {
-    return names[5];
-  } else {
-    return names[6];
-  }
-  // Only change code above this line
-}
+// function golfScore(par, strokes) {
+//   // Only change code below this line
+//   if (strokes == 1) {
+//     return names[0];
+//   } else if (strokes <= par - 2) {
+//     return names[1];
+//   } else if (strokes === par - 1) {
+//     return names[2];
+//   } else if (strokes === par) {
+//     return names[3];
+//   } else if (strokes === par + 1) {
+//     return names[4];
+//   } else if (strokes === par + 2) {
+//     return names[5];
+//   } else {
+//     return names[6];
+//   }
+//   // Only change code above this line
+// }
 
-// Change these values to test
-console.log(golfScore(5, 4));
+// // Change these values to test
+// console.log(golfScore(5, 4));
+// ------------------------------------------------
+// -----------return early pattern for function ----------
+
+// // Setup
+// function abTest(a, b) {
+//   // Only change code below this line
+//  if (a < 0 || b < 0){
+//    return undefined;;
+//  }
+ 
+
+//   // Only change code above this line
+
+//   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+// }
+
+// abTest(2,2);
+// console.log(abTest(2,2));
